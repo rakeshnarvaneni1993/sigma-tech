d75f59b2907050f5384197ebb9c9181b6983e3f5
Rakesh Narvaneni <narvaneni.rakesh2012@gmail.com>
2017-08-12 00:42:30 -0400

    Added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FrontPageComponent } from './components/front-page/front-page.component
 import { SiblingsToggleDirective } from './directives/siblings-toggle.directive';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { DataComponent } from './components/data/data.component';
+import { TestemonialsComponent } from './components/testemonials/testemonials.component';
+import { CaseStudiesComponent } from './components/case-studies/case-studies.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { DataComponent } from './components/data/data.component';
     FrontPageComponent,
     SiblingsToggleDirective,
     CarouselComponent,
-    DataComponent
+    DataComponent,
+    TestemonialsComponent,
+    CaseStudiesComponent
   ],
   imports: [
     BrowserModule,
